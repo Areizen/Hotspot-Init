1156b280ec09d2557367f02fee6af18f9e7fc154
Areizen <romain.kraft3@etu.univ-lorraine.fr>
2016-10-18 19:59:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo '\033[0;31m Installation du Firmware Wifi, Sudo et Nano \033[0m'
+echo -e '\033[0;31m Installation du Firmware Wifi, Sudo et Nano \033[0m'
 apt-get install sudo firmware-realtek nano
