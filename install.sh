297bb512fba5b4c5fc5450ffc46c754f3ab55c60
Areizen <romain.kraft3@etu.univ-lorraine.fr>
2016-10-18 20:22:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 echo -e '\033[0;31m Installation du Firmware Wifi, Sudo et Nano \033[0m'
 apt-get update
-apt-get install sudo firmware-realtek nano
+apt-get install sudo firmware-realtek usbutils nano
+
 echo -e '\033[0;31m Mise en place du Hotspot via le script de Raspberry-at-Home \033[0m'
 ifconfig wlan0 up
 wget "http://raspberry-at-home.com/files/ap_setup.sh"
